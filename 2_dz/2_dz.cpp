f5a97f496774baa873910e1144f832edda12900c
Hellhound <hardoneloves@gmail.com>
2021-11-20 13:34:05 +0300

    Добавьте файлы проекта.

---
+++ 2_dz/2_dz.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+int main()
+{
+    const int a = 10, b = 10;
+    if (a == 10 && b == 10) {
+        std::cout << "true";
+    }
+    else if (a + b == 10) {
+        std::cout << "true";
+    }
+    else std::cout << "false";
+
+}
